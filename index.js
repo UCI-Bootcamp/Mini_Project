9d6938000c5fced22f45982209411417537d7d9b
EvanErickson <evanserickson@gmail.com>
2020-02-12 13:26:29 -0800

    made some progress

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 const fs = require('fs')
+
 const inquirer = require('inquirer')
+
 const prompt = inquirer.createPromptModule()
-let questions = prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?'
-  },
-  {
-    type: 'input',
-    name: 'location',
-    message: 'What is your location?'
-  },
-  {
-    type: 'input',
-    name: 'bio',
-    message: 'What is your bio?'
-  },
-  {
-    type: 'input',
-    name: 'linkedIn',
-    message: 'What is your LinkedIn URL?'
-  }
+
+const myHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>`
+
+
+prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: 'What is your location?'
+    },
+    {
+        type: 'input',
+        name: 'bio',
+        message: 'What is your bio?'
+    },
+    {
+        type: 'input',
+        name: 'linkedIn',
+        message: 'What is your LinkedIn URL?'
+    }
 ])
 
-.then(({questions}) => {
-    console.log(({name}))
-    
-})
+    .then(({ name, location, bio, linkedIn }) => {
+        console.log(name, location, bio, linkedIn)
+        fs.appendFile('index.html', myHTML, () => {
+            console.log('hi')
+        })
+
+    })
 
 
 
